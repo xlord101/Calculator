bee71a0026904c383f67550e5f03afe703117aaf
Rugved Shelke <133494695+xlord101@users.noreply.github.com>
2025-02-24 09:56:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,68 @@
-let display = document.getElementById('display');
-let currentInput = '';
-let operator = '';
-let firstNumber = '';
-let secondNumber = '';
+function AC(){
 
-function appendNumber(number) {
-    currentInput += number;
-    display.value = currentInput;
+    let display1 = document.getElementById('display1');
+    display1.value = '';
+    let display2 = document.getElementById('display2');
+    display2.value = '';
+
 }
 
-function appendOperator(op) {
-    if (firstNumber === '') {
-        firstNumber = currentInput;
-        operator = op;
-        currentInput = '';
-    }
+function DEL(){
+
+    let display1 = document.getElementById('display1');
+    display1.value = display1.value.slice(0, -1);
+
+    let display2 = document.getElementById('display2');
+    display2.value = display2.value.slice(0, -1);
+
 }
 
-function calculateResult() {
-    secondNumber = currentInput;
-    if (firstNumber !== '' && operator !== '' && secondNumber !== '') {
-        let result;
-        switch (operator) {
-            case '+':
-                result = parseFloat(firstNumber) + parseFloat(secondNumber);
-                break;
-            case '-':
-                result = parseFloat(firstNumber) - parseFloat(secondNumber);
-                break;
-            case '*':
-                result = parseFloat(firstNumber) * parseFloat(secondNumber);
-                break;
-            case '/':
-                if (secondNumber === '0') {
-                    result = 'Error: Division by zero';
-                } else {
-                    result = parseFloat(firstNumber) / parseFloat(secondNumber);
-                }
-                break;
-            default:
-                result = 'Error';
-                break;
-        }
-        display.value = result;
-        currentInput = result.toString();
-        firstNumber = '';
-        operator = '';
-        secondNumber = '';
-    }
+function add(){
+
+    let firstNumber = Number(document.getElementById('display1').value);
+    let secondNumber = Number(document.getElementById('display2').value);
+
+    let result = firstNumber + secondNumber;
+
+    alert("Addition is : " + result);
 }
 
-function clearDisplay() {
-    currentInput = '';
-    firstNumber = '';
-    secondNumber = '';
-    operator = '';
-    display.value = '';
+function sub(){
+
+    let firstNumber = Number(document.getElementById('display1').value);
+    let secondNumber = Number(document.getElementById('display2').value);
+
+    let result = firstNumber - secondNumber;
+
+    alert("Substraction is : " + result);
+}
+
+function mult(){
+
+    let firstNumber = Number(document.getElementById('display1').value);
+    let secondNumber = Number(document.getElementById('display2').value);
+
+    let result = firstNumber * secondNumber;
+
+    alert("Multiplication is : " + result);
+}
+
+function div(){
+
+    let firstNumber = Number(document.getElementById('display1').value);
+    let secondNumber = Number(document.getElementById('display2').value);
+
+    let result = firstNumber / secondNumber;
+
+    alert("Division is : " + result);
+}
+
+function mod(){
+
+    let firstNumber = Number(document.getElementById('display1').value);
+    let secondNumber = Number(document.getElementById('display2').value);
+
+    let result = firstNumber % secondNumber;
+
+    alert("Modulus is : " + result);
 }
